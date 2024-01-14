c72955e205c7465ab2f59bb5b881eb0ccd8adb80
Thanawatkky <108441591+Thanawatkky@users.noreply.github.com>
2024-01-14 21:37:59 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = new mysqli("localhost","db_stc","user@2022!","db_stc");
+    $conn = new mysqli("localhost","root","","db_stc");
     ini_set("display_errors", 1);
     $conn->set_charset("utf8");
     header('Content-Type: text/html; charset=utf-8');
